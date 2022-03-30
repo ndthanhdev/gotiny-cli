9daf08eb9dae308939251c1b70955ed048ee1723
Duy Thanh Nguyen <ndthanhdev@outlook.com>
2022-03-31 01:07:05 +0700

    update installation + chore (#7)
    * update mac installation
    
    * update build scripts

--- misc/scripts/build.mjs
+++ misc/scripts/build.mjs
@@ -22,13 +22,8 @@ const argv = yargs(hideBin(process.argv))
 	.options("out", {
 		default: "./out",
 	})
-	.option("makeTar", {
-		type: "boolean",
-		demandOption: false,
-		default: false,
-	})
-	.option("genFormula", {
-		type: "boolean",
+	.option("local", {
+		type: 'boolean',
 		demandOption: false,
 		default: false,
 	})
@@ -46,8 +41,6 @@ import * as R from "ramda";
 
 const OSs = ["windows", "linux", "darwin"];
 const ARCHs = ["386", "amd64", "arm", "arm64"];
-
-let tapLog = (name) => R.tap((value) => console.log(name, value));
 
 function buildBinName(meta) {
 	let binName = "";
@@ -108,173 +101,5 @@ async function buildAll(metas) {
 }
 
 await buildAll(metas);
-
-async function makeTars(metas) {
-	metas = R.map((meta) => {
-		let tarPath = meta.outPath + ".tar.gz";
-
-		return {
-			...meta,
-			tarPath,
-		};
-	}, metas);
-
-	console.table(metas, ["binName", "tarPath"]);
-
-	await Promise.all(metas.map(makeTar));
-
-	return metas;
-
-	async function makeTar(meta) {
-		if (!["linux", "darwin"].includes(meta.os)) {
-			return;
-		}
-
-		console.log("making tarball for", meta.binName);
-
-		await $`tar -czf ${meta.tarPath} ${meta.outPath}`.pipe(process.stdout);
-		console.log("done:", meta.binName);
-	}
-}
-
-if (argv.makeTar) {
-	console.log("making tarball");
-	metas = await makeTars(metas);
-} else {
-	console.log("skipping making tarball");
-}
-
-import crypto from "crypto";
-import fs from "fs";
-
-async function genHash(metas) {
-	async function genOne(path) {
-		const fileBuffer = fs.readFileSync(path);
-		const hashSum = crypto.createHash("sha256");
-		hashSum.update(fileBuffer);
-		return hashSum.digest("hex");
-	}
-
-	metas = [...metas];
-
-	let ps = metas.map(async (meta) => {
-		let m = meta;
-		m.hash = await genOne(m.outPath);
-	});
-
-	await Promise.all(ps);
-
-	return metas;
-}
-
-async function genFormula(metas) {
-	metas = await genHash(metas);
-
-	console.table(metas, ["binName", "hash"]);
-
-	let d = R.pipe(
-		R.groupBy((meta) => meta.os),
-		R.map(
-			R.pipe(
-				R.groupBy((meta) => meta.arch),
-				R.map(R.nth(0))
-			)
-		)
-	)(metas);
-
-	console.log(JSON.stringify(d, null, 2));
-	let ver = argv.ver;
-
-	const formula = `class Gotiny < Formula
-    desc "Using gotiny.cc from the command-line"
-    homepage "https://github.com/ndthanhdev/gotiny-cli"
-    version "${ver.replace(/v/g, "")}"
-    license "MIT"
-
-    if OS.mac?
-      if Hardware::CPU.arm?
-        url "https://github.com/ndthanhdev/gotiny-cli/releases/download/${ver}/${
-		d.darwin.arm64.binName
-	}"
-        sha256 "${d.darwin.arm64.hash}"
-      else
-        url "https://github.com/ndthanhdev/gotiny-cli/releases/download/${ver}/${
-		d.darwin.amd64.binName
-	}"
-        sha256 "${d.darwin.amd64.hash}"
-      end
-    end
-
-    if OS.linux?
-      if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-        url "https://github.com/ndthanhdev/gotiny-cli/releases/download/${ver}/${
-		d.linux.arm64.binName
-	}"
-        sha256 "${d.linux.arm64.hash}"
-      elsif Hardware::CPU.arm?
-        url "https://github.com/ndthanhdev/gotiny-cli/releases/download/${ver}/${
-		d.linux.arm.binName
-	}"
-        sha256 "${d.linux.arm.hash}"
-      elsif Hardware::CPU.is_64_bit?
-        url "https://github.com/ndthanhdev/gotiny-cli/releases/download/${ver}/${
-		d.linux.amd64.binName
-	}"
-        sha256 "${d.linux.amd64.hash}"
-      else
-        url "https://github.com/ndthanhdev/gotiny-cli/releases/download/${ver}/${
-		d.linux["386"].binName
-	}"
-        sha256 "${d.linux["386"].hash}"
-      end
-    end
-
-    def install
-      if OS.mac?
-        if Hardware::CPU.arm?
-          bin.install "gotiny-${ver}-darwin-arm64" => "gotiny"
-        else
-          bin.install "gotiny-${ver}-darwin-amd64" => "gotiny"
-        end
-      end
-
-      if OS.linux?
-        if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-          bin.install "gotiny-${ver}-linux-arm64" => "gotiny"
-        elsif Hardware::CPU.arm?
-          bin.install "gotiny-${ver}-linux-arm" => "gotiny"
-        elsif Hardware::CPU.is_64_bit?
-          bin.install "gotiny-${ver}-linux-amd64" => "gotiny"
-        else
-          bin.install "gotiny-${ver}-linux-386" => "gotiny"
-        end
-      end
-    end
-
-    test do
-      system bin/"gotiny", "--version"
-    end
-  end
-`;
-
-	let file = `${OUT_DIR}/gotiny.rb`;
-	console.log("writing formula to", file);
-	console.log(formula);
-	fs.writeFileSync(file, formula, "utf-8");
-	console.log("write done");
-
-	return metas;
-}
-
-if (argv.genFormula) {
-	if (!argv.ver) {
-		console.log("missing version");
-	} else {
-		console.log("generating formula");
-		metas = genFormula(metas);
-	}
-} else {
-	console.log("skipping generating formula");
-}
 
 console.log("done");
